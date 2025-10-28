8e91c626df4a7a98a66366223458ba4e1b5ebc35
Juho Lee <wngh1716@gmail.com>
2025-10-28 13:34:53 +0900

    수정

--- src/main/java/com/example/board/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/board/exception/GlobalExceptionHandler.java
@@ -5,14 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice  // @RestController에서 발생하는 예외를 자동으로 잡아 처리
 public class GlobalExceptionHandler {
 
-    // 500 Internal Server Error 처리 (예상치 못한 모든 예외)
+    // 500 Internal Server Error 처리
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        // 서버 로그 기록
-        e.printStackTrace();
+
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("서버 오류가 발생했습니다: " + e.getMessage());
     }
