0d67d23dfa92542ef26ead8cb647b35d9ed05cda
Juho Lee <wngh1716@gmail.com>
2025-10-30 15:19:51 +0900

    수정

--- src/main/java/com/example/board/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/board/exception/GlobalExceptionHandler.java
@@ -8,6 +8,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice  // @RestController에서 발생하는 예외를 자동으로 잡아 처리
 public class GlobalExceptionHandler {
 
+    // 400 Error 처리
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequestException(Exception e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("잘못된 요청입니다." + e.getMessage());
+    }
+
+    // 400 Error 처리
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(Exception e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("잘못된 요청입니다." + e.getMessage());
+    }
+
     // 500 Internal Server Error 처리
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
